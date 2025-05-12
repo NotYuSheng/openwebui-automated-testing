b0f9b20cdd11f75579e3b1a57f89a2d8a7dcf652
xiaokaijie1 <calendar2074@gmail.com>
2025-05-12 10:42:13 +0800

    Update test_native_tool.py

--- test_native_tool.py
+++ test_native_tool.py
@@ -1,4 +1,4 @@
-afrom selenium import webdriver
+from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
